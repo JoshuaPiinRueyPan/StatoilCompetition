ccdd22ea0e9306575990d273e2c6715281185b94
JoshuaPan <piinrueypan@gmail.com>
2017-11-23 16:45:58 +0800

    [Runable] change Name in LayerHelper

--- src/LayerHelper.py
+++ src/LayerHelper.py
@@ -5,11 +5,12 @@ def ConvLayer(name, x, W, b, strides=1, padding='SAME'):
 	x = tf.nn.bias_add(x, b)
 	return tf.nn.relu(x, name = name)
 
-def MaxPoolLayer(name, x, k=2):
-	return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME', name=name)
-
 def AlexNorm(name, l_input, lsize=4):
 	return tf.nn.lrn(l_input, lsize, bias=1.0, alpha=0.001/9.0, beta=0.75, name=name)
+
+def MaxPoolLayer(name, x, kernelSize=2):
+	return tf.nn.max_pool(x, ksize=[1, kernelSize, kernelSize, 1],
+				 strides=[1, kernelSize, kernelSize, 1], padding='SAME', name=name)
 
 
 def AvgPoolLayer(name, x, kernelSize):
