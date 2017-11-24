58549d3c1e384f53e07a1e665a07bc90528be8d8
JoshuaPan <piinrueypan@gmail.com>
2017-11-24 13:07:49 +0800

    [Runable] Enlarge the FullConvAlexnetTiny

---
+++ src/subnet/AlexnetTinyWithoutAngle.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+from src.subnet.SubnetBase import SubnetBase
+from src.LayerHelper import *
+import settings.OutputSettings as outSettings
+
+class AlexnetTinyWithoutAngle(SubnetBase):
+	def __init__(self, isTraining_, inputImage_, inputAngle_, groundTruth_):
+		self.isTraining = isTraining_
+		self.inputImage = inputImage_
+		self.inputAngle = inputAngle_
+		self.groundTruth = groundTruth_
+
+	def Build(self):
+		weights, biases = self.buildNetVariables()
+		return self.buildNetBody(weights, biases)
+
+	def buildNetVariables(self):
+		weights = {
+			'convW1': tf.Variable(tf.random_normal([3, 3, 2, 8], stddev=0.01)),
+			'convW2': tf.Variable(tf.random_normal([3, 3, 8, 16], stddev=0.01)),
+			'convW3': tf.Variable(tf.random_normal([3, 3, 16, 16], stddev=0.01)),
+			'convW4': tf.Variable(tf.random_normal([3, 3, 16, 16], stddev=0.01)),
+			'fcW1'  : tf.Variable(tf.random_normal([4*4*16, 128], stddev=0.01)),
+			'fcW2'  : tf.Variable(tf.random_normal([128, 128], stddev=0.01)),
+			'output': tf.Variable(tf.random_normal([128, outSettings.NUMBER_OF_CATEGORIES], stddev=0.01)),
+		}
+
+		biases = {
+			'convb1': tf.Variable(tf.random_normal([8], stddev=0.01)),
+			'convb2': tf.Variable(tf.random_normal([16], stddev=0.01)),
+			'convb3': tf.Variable(tf.random_normal([16], stddev=0.01)),
+			'convb4': tf.Variable(tf.random_normal([16], stddev=0.01)),
+			'fcb1'  : tf.Variable(tf.random_normal([128], stddev=0.01)),
+			'fcb2'  : tf.Variable(tf.random_normal([128], stddev=0.01)),
+			'output'  : tf.Variable(tf.random_normal([outSettings.NUMBER_OF_CATEGORIES], stddev=0.01)),
+		}
+		return weights, biases
+
+	def buildNetBody(self, weights, biases):
+		conv1 = ConvLayer(self.inputImage, weights['convW1'], biases['convb1'], name='conv1')
+		pool1 = MaxPoolLayer(conv1, kernelSize=2, name='pool1')
+		norm1 = AlexNorm(pool1, lsize=4, name='norm1')
+
+		conv2 = ConvLayer(norm1, weights['convW2'], biases['convb2'], name='conv2')
+		pool2 = MaxPoolLayer(conv2, kernelSize=2, name='pool2')
+		norm2 = AlexNorm(pool2, lsize=4, name='norm3')
+
+		conv3 = ConvLayer(norm2, weights['convW3'], biases['convb3'], name='conv3')
+		pool3 = MaxPoolLayer(conv3, kernelSize=2, name='pool3')
+		norm3 = AlexNorm(pool3, lsize=4, name='norm3')
+
+		conv4 = ConvLayer(norm3, weights['convW4'], biases['convb4'], padding='VALID', name='conv4')
+		pool4 = MaxPoolLayer(conv4, kernelSize=2, name='pool4')
+		norm4 = AlexNorm(pool4, lsize=4, name='norm4')
+
+		pool4reshape = tf.reshape(norm4, [-1, weights['fcW1'].get_shape().as_list()[0]])
+
+		fc1 = tf.add(tf.matmul(pool4reshape, weights['fcW1']), biases['fcb1'])
+		fc1 = tf.nn.relu(fc1)
+
+		dropout = tf.cond(self.isTraining, lambda: 0.5, lambda: 1.)
+		fc1 = tf.nn.dropout(fc1, dropout)
+
+		fc2 = tf.reshape(fc1, [-1, weights['fcW2'].get_shape().as_list()[0]])
+		fc2 = tf.add(tf.matmul(fc2, weights['fcW2']), biases['fcb2'])
+		fc2 = tf.nn.relu(fc2)
+		fc2 = tf.nn.dropout(fc2, dropout)
+
+		output = tf.add(tf.matmul(fc2, weights['output']), biases['output'])
+		return output
+
--- src/subnet/FullConvAlexnetTiny.py
+++ src/subnet/FullConvAlexnetTiny.py
@@ -24,45 +24,55 @@ class FullConvAlexnetTiny(SubnetBase):
 
 	def buildNetVariables(self):
 		weights = {
-			'convW1': tf.Variable(tf.random_normal([3, 3, 2, 8], stddev=0.01)),
-			'convW2': tf.Variable(tf.random_normal([3, 3, 8, 16], stddev=0.01)),
-			'convW3': tf.Variable(tf.random_normal([3, 3, 16, 16], stddev=0.01)),
-			'convW4': tf.Variable(tf.random_normal([3, 3, 16, 16], stddev=0.01)),
-			'convW1x1' : tf.Variable(tf.random_normal([1, 1, 16, 2], stddev=0.01)),
+			'convW1': tf.Variable(tf.random_normal([3, 3, 2, 32], stddev=0.01)),
+			'convW2': tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01)),
+			'convW3': tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01)),
+			'convW4': tf.Variable(tf.random_normal([1, 1, 128, 64], stddev=0.01)),
+			'convW5': tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01)),
+			'convW6': tf.Variable(tf.random_normal([3, 3, 128, 256], stddev=0.01)),
+			'convW1x1' : tf.Variable(tf.random_normal([1, 1, 256, 2], stddev=0.01)),
 		}
 
 		biases = {
-			'convb1': tf.Variable(tf.random_normal([8], stddev=0.01)),
-			'convb2': tf.Variable(tf.random_normal([16], stddev=0.01)),
-			'convb3': tf.Variable(tf.random_normal([16], stddev=0.01)),
-			'convb4': tf.Variable(tf.random_normal([16], stddev=0.01)),
+			'convb1': tf.Variable(tf.random_normal([32], stddev=0.01)),
+			'convb2': tf.Variable(tf.random_normal([64], stddev=0.01)),
+			'convb3': tf.Variable(tf.random_normal([128], stddev=0.01)),
+			'convb4': tf.Variable(tf.random_normal([64], stddev=0.01)),
+			'convb5': tf.Variable(tf.random_normal([128], stddev=0.01)),
+			'convb6': tf.Variable(tf.random_normal([256], stddev=0.01)),
 			'convb1x1' : tf.Variable(tf.random_normal([2], stddev=0.01)),
 		}
 		return weights, biases
 
 	def buildNetBody(self, weights, biases):
-		conv1 = ConvLayer(self.inputImage, weights['convW1'], biases['convb1'], name='conv1')
-		pool1 = MaxPoolLayer(conv1, kernelSize=2, name='pool1')
-		norm1 = AlexNorm(pool1, lsize=4, name='norm1')
+		net = ConvLayer(self.inputImage, weights['convW1'], biases['convb1'], name='conv1')
+		net = MaxPoolLayer(net, kernelSize=2)
+		net = AlexNorm(net, lsize=4)
 
-		conv2 = ConvLayer(norm1, weights['convW2'], biases['convb2'], name='conv2')
-		pool2 = MaxPoolLayer(conv2, kernelSize=2, name='pool2')
-		norm2 = AlexNorm(pool2, lsize=4, name='norm3')
+		net = ConvLayer(net, weights['convW2'], biases['convb2'], name='conv2')
+		net = MaxPoolLayer(net, kernelSize=2)
+		net = AlexNorm(net, lsize=4)
 
-		conv3 = ConvLayer(norm2, weights['convW3'], biases['convb3'], name='conv3')
-		pool3 = MaxPoolLayer(conv3, kernelSize=2, name='pool3')
-		norm3 = AlexNorm(pool3, lsize=4, name='norm3')
+		net = ConvLayer(net, weights['convW3'], biases['convb3'], name='conv3')
+		net = AlexNorm(net, lsize=4)
 
-		conv4 = ConvLayer(norm3, weights['convW4'], biases['convb4'], name='conv4')
-		pool4 = MaxPoolLayer(conv4, kernelSize=2, name='pool4')
-		norm4 = AlexNorm(pool4, lsize=4, name='norm4')
+		net = ConvLayer(net, weights['convW4'], biases['convb4'], name='conv4')
+		net = AlexNorm(net, lsize=4)
 
-		conv1x1 = ConvLayer(norm4, weights['convW1x1'], biases['convb1x1'], padding='VALID', name='conv1x1')
-		avgPoolFinal = AvgPoolLayer(conv1x1, kernelSize=5, name='poolFinal')
-		output = tf.reshape(avgPoolFinal, shape=[-1, 2])
+		net = ConvLayer(net, weights['convW5'], biases['convb5'], name='conv5')
+		net = MaxPoolLayer(net, kernelSize=2)
+		net = AlexNorm(net, lsize=4)
 
-		#maxPoolFinal = MaxPoolLayer('poolFinal', conv1x1, kernelSize=5)
-		#output = tf.reshape(maxPoolFinal, shape=[-1, 2])
+		net = ConvLayer(net, weights['convW6'], biases['convb6'], name='conv6')
+		net = AlexNorm(net, lsize=4)
+
+		net = ConvLayer(net, weights['convW1x1'], biases['convb1x1'], padding='VALID', name='conv1x1')
+
+		print("conv.shape = " + str(net.shape))
+		#net = AvgPoolLayer(net, kernelSize=10, name='poolFinal')
+		net = MaxPoolLayer(net, kernelSize=10, name='poolFinal')
+		output = tf.reshape(net, shape=[-1, 2])
+
 		
 		return output
 
