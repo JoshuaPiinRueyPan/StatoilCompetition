5266c67eb776a41c840521a0a7607dadc3a953e9
JoshuaPan <piinrueypan@gmail.com>
2017-12-04 12:22:14 +0800

    [Runable] Refactor

--- src/DataAugmentation.py
+++ src/DataAugmentation.py
@@ -3,56 +3,82 @@ import numpy as np
 import settings.DataAugmentSettings as dataAugmentSettings
 
 class DataAugmentation:
+	def Augment(self, image):
+		if dataAugmentSettings.FLIP:
+			image = self.Flip(image)
+		if dataAugmentSettings.ZOOM:
+			image = self.Zoom(image)
+		if dataAugmentSettings.SHIFT:
+			image = self.Shift(image)
+		if dataAugmentSettings.ROTATE:
+			image = self.Rotate(image)
+		return image
+
 	def __init__(self):
 		pass
 
-	def _HorizontalFlip(self, image, u=0.5, v=1.0):   
-		if v < u:
-			image = cv2.flip(image, 1)
+	def Flip(self, image, PROBILITY_THRESHOLD_=0.5):
+		image = self._horizontalFlip(image, PROBILITY_THRESHOLD_)
+		image = self._verticalFlip(image, PROBILITY_THRESHOLD_)
 		return image
 
-	def _VerticalFlip(self, image, u=0.5, v=1.0):
-		if v < u:
-			image = cv2.flip(image, 0)
-		return image
 
-	def Flip(self, image):
-		image = self._HorizontalFlip(image, u=0.5, v=np.random.random())
-		image = self._VerticalFlip(image, u=0.5, v=np.random.random())
-		return image
 
-	def _HorizontalShift(self, image, u=0.33, v=1.0):
-		rightShift = np.float32([[1,0,15],[0,1,0]])
-		leftShift = np.float32([[1,0,-15],[0,1,0]])
-		rows,cols = image.shape[:2]
-		if v < u:
-			image = cv2.warpAffine(image, rightShift, (rows, cols))
-		elif v < 2*u:
-			image = cv2.warpAffine(image, leftShift, (rows, cols))
-		return image
+	def Zoom(self, image, PROBILITY_THRESHOLD_=0.33):
+		randomValue = np.random.random()
+		if randomValue < PROBILITY_THRESHOLD_:
+			image = self._zoomin(image)
 
-	def _VerticalShift(self, image, u=0.33, v=1.0):
-		downShift = np.float32([[1,0,0],[0,1,15]])
-		upShift = np.float32([[1,0,0],[0,1,-15]])
-		rows,cols = image.shape[:2]
-		if v < u:
-			image = cv2.warpAffine(image, downShift, (rows, cols))
-		elif v < 2*u:
-			image = cv2.warpAffine(image, upShift, (rows, cols))
+		elif randomValue < 2*PROBILITY_THRESHOLD_:
+			image = self._zoomout(image)
+
 		return image
 
 	def Shift(self, image):
-		image = self._HorizontalShift(image, u=0.33, v=np.random.random())
-		image = self._VerticalShift(image, u=0.33, v=np.random.random())
+		image = self._horizontalShift(image, PROBILITY_THRESHOLD_=0.33)
+		image = self._verticalShift(image, PROBILITY_THRESHOLD_=0.33)
 		return image
 
 	def Rotate(self, image):
-		u = 0.125
-		v = np.random.random()
+		randomValue = np.random.random()
 		rows,cols = image.shape[:2]
-		angle = int(v/u)
-		RotationMatrix = cv2.getRotationMatrix2D((cols/2, rows/2), angle*45, 1)
+		angle = randomValue  * 360.0
+		RotationMatrix = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
 		image = cv2.warpAffine(image, RotationMatrix, (rows,cols))
+		return image
+
+	def _horizontalFlip(self, image, PROBILITY_THRESHOLD_=0.5):
+		randomValue = np.random.random()
+		if randomValue < PROBILITY_THRESHOLD_:
+			image = cv2.flip(image, 1)
+		return image
+
+	def _verticalFlip(self, image, PROBILITY_THRESHOLD_=0.5):
+		randomValue = np.random.random()
+		if randomValue < PROBILITY_THRESHOLD_:
+			image = cv2.flip(image, 0)
+		return image
+
+	def _horizontalShift(self, image, PROBILITY_THRESHOLD_=0.33):
+		randomValue = np.random.random()
+		rightShift = np.float32([[1,0,15],[0,1,0]])
+		leftShift = np.float32([[1,0,-15],[0,1,0]])
+		rows,cols = image.shape[:2]
+		if randomValue < PROBILITY_THRESHOLD_:
+			image = cv2.warpAffine(image, rightShift, (rows, cols))
+		elif randomValue < 2*PROBILITY_THRESHOLD_:
+			image = cv2.warpAffine(image, leftShift, (rows, cols))
+		return image
+
+	def _verticalShift(self, image, PROBILITY_THRESHOLD_=0.33):
+		randomValue = np.random.random()
+		downShift = np.float32([[1,0,0],[0,1,15]])
+		upShift = np.float32([[1,0,0],[0,1,-15]])
+		rows,cols = image.shape[:2]
+		if randomValue < PROBILITY_THRESHOLD_:
+			image = cv2.warpAffine(image, downShift, (rows, cols))
+		elif randomValue < 2*PROBILITY_THRESHOLD_:
+			image = cv2.warpAffine(image, upShift, (rows, cols))
 		return image
 
 	def _zoomin(self, image):
@@ -72,24 +98,6 @@ class DataAugmentation:
 		imagezero = cv2.resize(imagezero, (rows,cols))
 		return imagezero
 
-	def Zoom(self, image, u=0.33):
-		v = np.random.random()
-		if v < u:
-			image = self._zoomin(image)
-		elif v < 2*u:
-			image = self._zoomout(image)
-		return image
-
-	def Augment(self, image):
-		if dataAugmentSettings.FLIP:
-			image = self.Flip(image)
-		if dataAugmentSettings.ZOOM:
-			image = self.Zoom(image)
-		if dataAugmentSettings.SHIFT:
-			image = self.Shift(image)
-		if dataAugmentSettings.ROTATE:
-			image = self.Rotate(image)
-		return image
 
 if __name__ == '__main__':
 	a = cv2.imread("Canvas1.jpg")
